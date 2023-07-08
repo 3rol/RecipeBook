995a772d011a75c36304284951cda0ba408b8edd
Ema <ema0kd@gmail.com>
2023-07-08 21:44:22 +0200

    added recipe-service

---
+++ assets/js/recipe-service.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent the form from submitting normally
+      RecipeService.addRecipe();
+    });
+  });
+
+  
+  var RecipeService = {
+    addRecipe: function (recipe) {
+      // Code to handle adding a recipe
+      var title = document.getElementById("recipeTitle").value;
+      var type = document.getElementById("recipeType").value;
+      var description = document.getElementById("description").value;
+      
+      var newRecipe = {
+        title: title,
+        type: type,
+        description: description
+      };
+  
+  
+      // Example AJAX code:
+
+      $.ajax({
+        url: "rest/recipes",
+        type: "POST",
+        data: JSON.stringify(newRecipe),
+        contentType: "application/json",
+        success: function (response) {
+          // Handle the success response
+          console.log("Recipe added successfully");
+        },
+        error: function (error) {
+          // Handle the error response
+          console.error("Failed to add recipe:", error);
+        }
+      });
+      
+      // Reset the form after adding the recipe
+      document.getElementById("recipeTitle").value = "";
+      document.getElementById("recipeType").value = "Choose";
+      document.getElementById("description").value = "";
+    }
+  };
+  
