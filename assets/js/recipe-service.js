9523ab530a5c81074f15b6a30ce6f91766a77a94
Ema <ema0kd@gmail.com>
2023-07-09 13:18:24 +0200

    added jQuery to recipe-service, updated index.html

--- assets/js/recipe-service.js
+++ assets/js/recipe-service.js
@@ -1,46 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var form = document.querySelector("form");
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-      RecipeService.addRecipe();
-    });
+$(document).ready(function() {
+  $("#recipeForm").submit(function(event) {
+    event.preventDefault();
+    RecipeService.addRecipe();
   });
+});
 
-  
-  var RecipeService = {
-    addRecipe: function (recipe) {
-      // Code to handle adding a recipe
-      var title = document.getElementById("recipeTitle").value;
-      var type = document.getElementById("recipeType").value;
-      var description = document.getElementById("description").value;
-      
-      var newRecipe = {
-        title: title,
-        type: type,
-        description: description
-      };
-  
+var RecipeService = {
+  addRecipe: function() {
+    var title = $("#recipeTitle").val();
+    var type = $("#recipeType").val();
+    var description = $("#description").val();
 
+    var newRecipe = {
+      title: title,
+      type: type,
+      description: description
+    };
 
-      $.ajax({
-        url: "rest/recipes",
-        type: "POST",
-        data: JSON.stringify(newRecipe),
-        contentType: "application/json",
-        success: function (response) {
-          // Handle the success response
-          console.log("Recipe added successfully");
-        },
-        error: function (error) {
-          // Handle the error response
-          console.error("Failed to add recipe:", error);
-        }
-      });
-      
-      // Reset the form after adding the recipe
-      document.getElementById("recipeTitle").value = "";
-      document.getElementById("recipeType").value = "Choose";
-      document.getElementById("description").value = "";
-    }
-  };
-  
+    $.ajax({
+      url: "rest/recipes",
+      type: "POST",
+      data: JSON.stringify(newRecipe),
+      contentType: "application/json",
+      success: function(response) {
+        console.log("Recipe added successfully");
+        // Reset the form after successful submission
+        $("#recipeForm")[0].reset();
+      },
+      error: function(error) {
+        console.error("Failed to add recipe:", error);
+      }
+    });
+  }
+};
